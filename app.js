a34a84e41f5fa724420c7d6398c12d76e56d3245
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-08-11 11:22:20 +0500

    Rock,Paper,Scissors Game with simple JS Functions.

---
+++ app.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll('button');
+const resultEle = document.getElementById('result');
+const playerScoreEle = document.getElementById('user-score');
+const computerScoreEle = document.getElementById('computer-score');
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const result = playRound(button.id, computerPlay());
+      resultEle.textContent = result;
+      
+    });
+});
+
+function computerPlay(){
+    const choices=["rock", "paper","scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice]; 
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return "It's a tie:";
+    }else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+    ){
+      playerScore++;
+      playerScoreEle.textContent = playerScore;
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        computerScore++;
+        computerScoreEle.textContent = computerScore;
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
